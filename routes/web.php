17295a04f26925eca3b6598d72c6814ca7023f9f
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2024-02-02 18:16:47 +0300

    Add Admin Guard

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('admin.layouts.master');
-    return view('admin.dashboard.index');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
