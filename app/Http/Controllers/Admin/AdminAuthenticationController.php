0a3b9c5e3e804807fea8fc05f7e1e0cf645215b3
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2024-02-03 21:07:48 +0300

    Make Reset Link for Admin

--- app/Http/Controllers/Admin/AdminAuthenticationController.php
+++ app/Http/Controllers/Admin/AdminAuthenticationController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\HandleLoginRequest;
+use App\Http\Requests\SendResetLinkRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Password;
 
 class AdminAuthenticationController extends Controller
 {
@@ -11,4 +15,47 @@ class AdminAuthenticationController extends Controller
     {
         return view('admin.auth.login');
     }
+
+    public function handleLogin(HandleLoginRequest $request)
+    {
+        $request->authenticate();
+        return redirect()->route('admin.dashboard');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('admin')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('admin.login');
+    }
+
+    public function forgotPassword()
+    {
+        return view('admin.auth.forgot-password');
+    }
+
+    public function sendResetLink(SendResetLinkRequest $request)
+    {
+        dd($request->all());
+        // $request->validate([
+        //     'email' => ['required', 'email'],
+        // ]);
+
+        // // We will send the password reset link to this user. Once we have attempted
+        // // to send the link, we will examine the response then see the message we
+        // // need to show to the user. Finally, we'll send out a proper response.
+        // $status = Password::sendResetLink(
+        //     $request->only('email')
+        // );
+
+        // return $status == Password::RESET_LINK_SENT
+        //             ? back()->with('status', __($status))
+        //             : back()->withInput($request->only('email'))
+        //                     ->withErrors(['email' => __($status)]);
+    }
 }
+
--- resources/views/admin/auth/forgot-password.blade.php
+++ resources/views/admin/auth/forgot-password.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Login &mdash; Stisla</title>
+    <title>Forgot Password &mdash; Stisla</title>
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{ asset('admin/assets') }}/modules/bootstrap/css/bootstrap.min.css">
@@ -45,51 +45,28 @@
 
                         <div class="card card-primary">
                             <div class="card-header">
-                                <h4>Login</h4>
+                                <h4>Forgot Password</h4>
                             </div>
 
                             <div class="card-body">
-                                <form method="POST" action="{{route('admin.handle-login')}}" class="needs-validation" novalidate="">
+                                <form method="POST" action="{{ route('admin.forgot-password.send') }}"
+                                    class="needs-validation" novalidate="">
                                     @csrf
                                     <div class="form-group">
                                         <label for="email">Email</label>
                                         <input id="email" type="email" class="form-control" name="email"
                                             tabindex="1" required autofocus>
-                                            @error('email')
-                                                <code>{{$message}}</code>
-                                            @enderror
+                                        @error('email')
+                                            <code>{{ $message }}</code>
+                                        @enderror
                                         <div class="invalid-feedback">
                                             Please fill in your email
                                         </div>
                                     </div>
 
                                     <div class="form-group">
-                                        <div class="d-block">
-                                            <label for="password" class="control-label">Password</label>
-                                            <div class="float-right">
-                                                <a href="auth-forgot-password.html" class="text-small">
-                                                    Forgot Password?
-                                                </a>
-                                            </div>
-                                        </div>
-                                        <input id="password" type="password" class="form-control" name="password"
-                                            tabindex="2" required>
-                                        <div class="invalid-feedback">
-                                            please fill in your password
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <div class="custom-control custom-checkbox">
-                                            <input type="checkbox" name="remember" class="custom-control-input"
-                                                tabindex="3" id="remember-me">
-                                            <label class="custom-control-label" for="remember-me">Remember Me</label>
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group">
                                         <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
-                                            Login
+                                            Send Link
                                         </button>
                                     </div>
                                 </form>
