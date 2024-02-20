9246499e68df4b2a17ea28792dc66fbeb80438a1
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2024-02-20 18:34:30 +0300

    Complete Update And Delete Feature For Social Counts

---
+++ resources/views/admin/social-count/edit.blade.php
@@ -0,0 +1,103 @@
+@extends('admin.layouts.master')
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>{{ __('Social Count') }}</h1>
+        </div>
+
+        <div class="card card-primary">
+            <div class="card-header">
+                <h4>{{ __('Create Social Link') }}</h4>
+
+            </div>
+            <div class="card-body">
+                <form action="{{ route('admin.social-count.update', $socialCount->id) }}" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <div class="form-group">
+                        <label for="">{{ __('Language') }}</label>
+                        <select name="language" id="language-select" class="form-control select2">
+                            <option value="">{{ __('Select') }}</option>
+                            @foreach ($languages as $lang)
+                                <option {{$lang->lang === $socialCount->language ? 'selected': '' }} value="{{ $lang->lang }}">{{ $lang->name }}</option>
+                            @endforeach
+                        </select>
+                        @error('language')
+                            <p class="text-danger">{{ $message }}</p>
+                        @enderror
+
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">{{ __('Icon') }}</label><br>
+                        <button class="btn btn-primary" role="iconpicker" name="icon" data-icon="{{$socialCount->icon}}"></button>
+                        @error('icon')
+                            <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">{{ __('Url') }}</label>
+                        <input name="url" type="text" class="form-control" id="name" value="{{$socialCount->url}}">
+                        @error('url')
+                            <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">{{ __('Fan Count') }}</label>
+                        <input name="fan_count" type="text" class="form-control" id="name" value="{{$socialCount->fan_count}}">
+                        @error('fan_count')
+                            <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">{{ __('Fan Type') }}</label>
+                        <input name="fan_type" type="text" class="form-control" id="name"
+                        placeholder="ex: likes, fans, followers" value="{{$socialCount->fan_type}}">
+                        @error('fan_type')
+                            <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">{{ __('Button Text') }}</label>
+                        <input name="button_text" type="text" class="form-control" id="name" value="{{$socialCount->button_text}}">
+                        @error('button_text')
+                            <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label>{{__('Pick Your Color')}}</label>
+                        <div class="input-group colorpickerinput">
+                          <input type="text" class="form-control" name="color" value="{{$socialCount->color}}">
+                          <div class="input-group-append">
+                            <div class="input-group-text">
+                              <i class="fas fa-fill-drip"></i>
+                            </div>
+                          </div>
+                        </div>
+                        @error('color')
+                            <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                      </div>
+
+                    <div class="form-group">
+                        <label for="">{{ __('Status') }}</label>
+                        <select name="status" id="" class="form-control">
+                            <option {{$socialCount->status === 1 ? 'selected' : ''}} value="1">{{ __('Active') }}</option>
+                            <option {{$socialCount->status === 0 ? 'selected' : ''}} value="0">{{ __('Inactive') }}</option>
+                        </select>
+                        @error('status')
+                            <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <button type="submit" class="btn btn-primary">{{ __('Update') }}</button>
+                </form>
+            </div>
+        </div>
+    </section>
+@endsection
