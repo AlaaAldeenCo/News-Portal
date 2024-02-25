7939fa4835852af0834f88d3d27399146713e591
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2024-02-25 23:24:26 +0300

    Send Email To Created User

--- app/Http/Controllers/Admin/RoleUserController.php
+++ app/Http/Controllers/Admin/RoleUserController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AdminRoleUserStoreRequest;
+use App\Mail\RoleUserCreateMail;
 use App\Models\Admin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Spatie\Permission\Models\Role;
 
 class RoleUserController extends Controller
@@ -32,19 +34,31 @@ class RoleUserController extends Controller
      */
     public function store(AdminRoleUserStoreRequest $request)
     {
-        $user = new Admin();
-        $user->image = '';
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = bcrypt($request->paassword);
-        $user->status = 1;
-        $user->save();
+        try
+        {
+            $user = new Admin();
+            $user->image = '';
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = bcrypt($request->paassword);
+            $user->status = 1;
+            $user->save();
 
-        /* Assign A Role To The User */
+            /* Assign A Role To The User */
 
-        $user->assignRole($request->role);
-        toast(__('Created Successfully'), 'success');
-        return redirect()->route('admin.role-users.index');
+            $user->assignRole($request->role);
+
+            Mail::to($request->email)->send(new RoleUserCreateMail($request->email, $request->password));
+
+            toast(__('Created Successfully'), 'success');
+            return redirect()->route('admin.role-users.index');
+
+        }
+        catch (\Throwable $th)
+        {
+            throw $th;
+        }
+
     }
 
     /**
