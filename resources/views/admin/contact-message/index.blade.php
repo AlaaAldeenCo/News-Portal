65ff75f24a78e4095d74d0b2832a5a6103a7c2b8
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2024-03-02 17:16:59 +0300

    Working With Replay Feature

--- resources/views/admin/contact-message/index.blade.php
+++ resources/views/admin/contact-message/index.blade.php
@@ -1,7 +1,6 @@
 @extends('admin.layouts.master')
 
 @section('content')
-
     <section class="section">
         <div class="section-header">
             <h1>{{ __('Social Links') }}</h1>
@@ -35,31 +34,29 @@
                         <tbody>
 
                             @foreach ($messages as $message)
-                            <tr>
-                                <td>{{$message->id}}</td>
-                                <td>{{$message->email}}</td>
-                                <td>{{$message->subject}}</td>
-                                <td>{{$message->message}}</td>
-                                <td>
-                                    @if ($message->replied == 1)
-                                    <i style="font-size:20px" class="fas fa-check text-success"></i>
-                                    @else
-                                    <i style="font-size:20px" class="fas fa-clock text-warning"></i>
-                                    @endif
+                                <tr>
+                                    <td>{{ $message->id }}</td>
+                                    <td>{{ $message->email }}</td>
+                                    <td>{{ $message->subject }}</td>
+                                    <td>{{ $message->message }}</td>
+                                    <td>
+                                        @if ($message->replied == 1)
+                                            <i style="font-size:20px" class="fas fa-check text-success"></i>
+                                        @else
+                                            <i style="font-size:20px" class="fas fa-clock text-warning"></i>
+                                        @endif
 
-                                </td>
-                                <td>
+                                    </td>
+                                    <td>
 
-                                    <a href=""
-                                        class="btn btn-primary"><i
-                                        class="fas fa-envelope"></i></a>
-                                    <a href="{{ route('admin.social-link.destroy', $message->id) }}"
-                                        class="btn btn-danger delete-item"><i
-                                            class="fas fa-trash-alt"></i></a>
+                                        <a href="" class="btn btn-primary" data-toggle="modal"
+                                            data-target="#exampleModal-{{$message->id}}"><i class="fas fa-envelope"></i></a>
+                                        <a href="{{ route('admin.social-link.destroy', $message->id) }}"
+                                            class="btn btn-danger delete-item"><i class="fas fa-trash-alt"></i></a>
 
-                                </td>
+                                    </td>
 
-                            </tr>
+                                </tr>
                             @endforeach
 
 
@@ -75,16 +72,57 @@
 
         </div>
     </section>
+    <!-- Modal -->
+    @foreach ( $messages as $message )
+    <div class="modal fade" id="exampleModal-{{$message->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">{{__('Replay To')}}: {{$message->email}}</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="">{{ __('Subject') }}</label>
+                        <input type="text" name="subject" id="" class="form-control">
+                        <input type="hidden" name="email" value="{{ $message->email }}" id=""
+                            class="form-control">
+                        <input type="hidden" name="message_id" value="{{ $message->id }}" id=""
+                        class="form-control">
+                        @error('subject')
+                            <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="">{{ __('Message') }}</label>
+                        <textarea name="message" class="form-control" style="height: 200px !important;"></textarea>
+                        @error('message')
+                            <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary"
+                            data-dismiss="modal">{{ __('Close') }}</button>
+                        <button type="submit" class="btn btn-primary">{{ __('Send') }}</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endforeach
+
 @endsection
 
 @push('scripts')
     <script>
-            $("#table-sub").dataTable({
-                "columnDefs": [{
-                    "sortable": false,
-                    "targets": [2, 3]
-                }]
-            });
-
+        $("#table-sub").dataTable({
+            "columnDefs": [{
+                "sortable": false,
+                "targets": [2, 3]
+            }]
+        });
     </script>
 @endpush
