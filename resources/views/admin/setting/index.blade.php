28b453c748566cd057fe350f59843db6570bd905
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2024-03-03 11:26:15 +0300

    Working With general Settings

--- resources/views/admin/setting/index.blade.php
+++ resources/views/admin/setting/index.blade.php
@@ -36,15 +36,19 @@
                                     @method('PUT')
                                     <div class="form-group">
                                         <label>{{__('Site Name')}}</label>
-                                        <input class="form-control" type="text" name="site_name">
+
+                                        <input class="form-control" type="text" name="site_name" value="{{$settings['site_name']}}">
                                     </div>
 
                                     <div class="form-group">
+                                        <img src="{{asset($settings['site_logo'])}}" width="150px"><br>
                                         <label>{{__('Site Logo')}}</label>
+
                                         <input class="form-control" type="file" name="site_logo">
                                     </div>
 
                                     <div class="form-group">
+                                        <img src="{{asset($settings['site_favicon'])}}" width="150px"><br>
                                         <label>{{__('Site Favicon')}}</label>
                                         <input class="form-control" type="file" name="site_favicon">
                                     </div>
