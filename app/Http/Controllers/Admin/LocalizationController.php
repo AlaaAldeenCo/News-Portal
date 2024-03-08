5cc36c97c82213659c7b659aa5332c1d42a0bd62
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2024-03-08 17:22:36 +0300

    Working With Microsoft Translation API

--- app/Http/Controllers/Admin/LocalizationController.php
+++ app/Http/Controllers/Admin/LocalizationController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Language;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Http;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 
@@ -31,60 +32,85 @@ class LocalizationController extends Controller
 
         $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory));
         $localizationStrings = [];
-        foreach($files as $file)
-        {
-            if($file->isDir())
-            {
+        foreach ($files as $file) {
+            if ($file->isDir()) {
                 continue;
             }
             $contents = file_get_contents($file->getPathname());
             preg_match_all('/__\([\'"](.+?)[\'"]\)/', $contents, $matches);
-            if(!empty($matches[1]))
-            {
-                foreach($matches[1] as $match)
-                {
+            if (!empty($matches[1])) {
+                foreach ($matches[1] as $match) {
                     $localizationStrings[$match] = $match;
                 }
             }
-
         }
         $phpArray = "<?php\n\nreturn " . var_export($localizationStrings, true) . ";\n";
 
-        if(!File::isDirectory(lang_path($langaugeCode)))
-        {
+        if (!File::isDirectory(lang_path($langaugeCode))) {
             File::makeDirectory(lang_path($langaugeCode), 0755, true);
         }
         // dd(lang_path($langaugeCode.'/'.$fileName.'.php'));
-        file_put_contents(lang_path($langaugeCode.'/'.$fileName.'.php'), $phpArray);
+        file_put_contents(lang_path($langaugeCode . '/' . $fileName . '.php'), $phpArray);
     }
 
     public function updateLangString(Request $request)
     {
         $languageStrings = trans($request->file_name, [], $request->lang_code);
-        $languageStrings [$request->key] = $request->value;
+        $languageStrings[$request->key] = $request->value;
         $phpArray = "<?php\n\nreturn " . var_export($languageStrings, true) . ";\n";
-        file_put_contents(lang_path($request->lang_code.'/'.$request->file_name.'.php'), $phpArray);
+        file_put_contents(lang_path($request->lang_code . '/' . $request->file_name . '.php'), $phpArray);
         toast(__('Updated Successfully!'), 'success');
         return redirect()->back();
     }
 
     public function translateString(Request $request)
     {
-        $client = new \GuzzleHttp\Client();
+        // $client = new \GuzzleHttp\Client();
 
-        $response = $client->request('POST', 'https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=%3CREQUIRED%3E&api-version=3.0&profanityAction=NoAction&textType=plain', [
-            'body' => '[
-            {
-                "Text": "I would really like to drive your car around the block a few times."
-            }
-        ]',
-            'headers' => [
-                'X-RapidAPI-Host' => 'microsoft-translator-text.p.rapidapi.com',
-                'X-RapidAPI-Key' => '1b5d1d17aamsha9cae4a30ef4f68p1b7e92jsn80425bbdbb47',
-                'content-type' => 'application/json',
-            ],
-        ]);
+        // $response = $client->request('POST', 'https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=%3CREQUIRED%3E&api-version=3.0&profanityAction=NoAction&textType=plain', [
+        //     'body' => '[
+        //     {
+        //         "Text": "I would really like to drive your car around the block a few times."
+        //     }
+        // ]',
+        //     'headers' => [
+        //         'X-RapidAPI-Host' => 'microsoft-translator-text.p.rapidapi.com',
+        //         'X-RapidAPI-Key' => '1b5d1d17aamsha9cae4a30ef4f68p1b7e92jsn80425bbdbb47',
+        //         'content-type' => 'application/json',
+        //     ],
+        // ]);
 
-        echo $response->getBody();
+        $response = Http::withHeaders([
+            'X-RapidAPI-Host' => 'microsoft-translator-text.p.rapidapi.com',
+            'X-RapidAPI-Key' => '1b5d1d17aamsha9cae4a30ef4f68p1b7e92jsn80425bbdbb47',
+            'content-type' => 'application/json',
+        ])->post(
+            'https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=ar&api-version=3.0&profanityAction=NoAction&textType=plain',
+                [
+                    [
+                        "Text" => "I would really like to drive your car around the block a few times."
+                    ]
+                ]
+        );
+        return $response->body();
+
+//         <?php
+
+// $client = new \GuzzleHttp\Client();
+
+// $response = $client->request('POST', 'https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=%3CREQUIRED%3E&api-version=3.0&profanityAction=NoAction&textType=plain', [
+// 	'body' => '[
+//     {
+//         "Text": "I would really like to drive your car around the block a few times."
+//     }
+// ]',
+// 	'headers' => [
+// 		'X-RapidAPI-Host' => 'microsoft-translator-text.p.rapidapi.com',
+// 		'X-RapidAPI-Key' => '1b5d1d17aamsha9cae4a30ef4f68p1b7e92jsn80425bbdbb47',
+// 		'content-type' => 'application/json',
+// 	],
+// ]);
+
+// echo $response->getBody();
     }
 }
