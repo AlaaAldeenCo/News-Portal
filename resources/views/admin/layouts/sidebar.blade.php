e8269dbc21bfade214a3357a00695a6f7356192d
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2024-03-11 00:13:37 +0300

    Working With Dashboard Page

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -11,7 +11,7 @@
         <ul class="sidebar-menu">
             <li class="menu-header">{{ __('admin.Dashboard') }}</li>
             <li class="active">
-                <a href="#" class="nav-link "><i class="fas fa-fire"></i><span>{{ __('admin.Dashboard') }}</span></a>
+                <a href="{{route('admin.dashboard')}}" class="nav-link "><i class="fas fa-fire"></i><span>{{ __('admin.Dashboard') }}</span></a>
 
             </li>
             <li class="menu-header">{{ __('admin.Starter') }}</li>
